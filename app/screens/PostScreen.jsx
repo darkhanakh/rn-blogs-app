52d6d1fb225ca38c592190d1a7a90975ec963139
Darkhan <darkhanahmetov228@gmail.com>
2021-03-22 21:43:09 +0600

    feat: added PostScreen template

--- app/screens/PostScreen.jsx
+++ app/screens/PostScreen.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Button, ScrollView, Alert } from 'react-native';
 import styled from 'styled-components/native';
+import DATA from '../data';
+import { THEME } from '../theme';
 
-const StyledView = styled(View)`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: 200px;
+`;
+
+const PostText = styled(Text)`
+  font-family: 'open-regular';
+`;
+
+const TextWrapper = styled(ScrollView)`
+  padding: 10px;
 `;
 
 export default function PostScreen({ navigation, route }) {
   const { postId } = route.params;
+  const post = DATA.find(p => p.id === postId);
+
+  const removePost = () => {
+    Alert.alert('Удаление поста', 'Вы уверены что хотите удалить пост?', [
+      {
+        text: 'Отменить',
+        style: 'cancel',
+      },
+      { text: 'Удалить', style: 'destructive', onPress() {} },
+    ]);
+  };
+
   return (
-    <StyledView>
-      <Text>{postId}</Text>
-    </StyledView>
+    <View>
+      <StyledImage
+        source={{
+          uri: post.img,
+        }}
+      />
+      <TextWrapper>
+        <PostText>{post.text}</PostText>
+      </TextWrapper>
+      <Button title="Удалить" color={THEME.DANGER_COLOR} onPress={removePost} />
+    </View>
   );
 }
